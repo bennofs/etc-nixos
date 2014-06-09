2e480f0d429c0d6422e3ccc64abbbf6239912819
Benno Fünfstück <benno.fuenfstueck@gmail.com>
2014-06-09 15:08:03 +0200

    Fix nix-head

--- expr/nix-head/default.nix
+++ expr/nix-head/default.nix
@@ -1,8 +1,14 @@
-{ stdenv, nix, fetchgit }:
+{ stdenv, nix, fetchgit, autoconf, automake, libxml2, libxslt, docbook5, docbook5_xsl, flex, bison, tetex, dblatex, git, w3m }:
 
 stdenv.lib.overrideDerivation nix (old: {
   src = fetchgit {
     url = https://github.com/NixOS/nix;
     sha256 = "ef09093e92a6fb4d9d2743b8ba1dab8f0c61e1cbb370459988e9135b7a3134c4";
   };
+  buildInputs = old.buildInputs ++ [ autoconf automake libxml2 libxslt flex bison tetex dblatex git w3m ];
+  preConfigure = "./bootstrap.sh";
+  configureFlags = ''
+    --with-docbook-rng=${docbook5}/xml/rng/docbook
+    --with-docbook-xsl=${docbook5_xsl}/xml/xsl/docbook
+  '' + old.configureFlags;
 })
