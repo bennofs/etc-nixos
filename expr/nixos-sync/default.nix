a7d9db49afbf852786e13e03f1a64195716ec771
Benno Fünfstück <benno.fuenfstueck@gmail.com>
2015-01-24 16:55:44 +0100

    Fix nixos-sync default.nix

--- expr/nixos-sync/default.nix
+++ expr/nixos-sync/default.nix
@@ -8,6 +8,6 @@ stdenv.mkDerivation {
     export PATH=/var/setuid-wrappers:${coreutils}/bin:${gnused}/bin:${less}/bin:${git}/bin:${nixos-rebuild}/bin
     exec ${stdenv.shell} ${./nixos-sync.sh}
     EOF
-    chmod +x $out/bin/nixos-sync
+    chmod +x $out
   '';
 }
