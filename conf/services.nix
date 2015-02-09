55676d86a0535a8237cbe283bbb361bec2627a6b
Benno Fünfstück <benno.fuenfstueck@gmail.com>
2015-02-09 13:37:08 +0100

    Disable hydra

--- conf/services.nix
+++ conf/services.nix
@@ -39,7 +39,7 @@ services = {
 
   # Setup Hydra-CI
   hydra = {
-    enable = true;
+    enable = false;
     package = expr.hydra;
     hydraURL = "c-cube";
     notificationSender = "benno.fuenfstueck@gmail.com";
