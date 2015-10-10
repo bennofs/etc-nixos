729181973ea0a58da44728f18d5ec276c4ee5e63
Benno Fünfstück <benno.fuenfstueck@gmail.com>
2015-10-11 00:02:09 +0200

    add hardware-overrides for system-specific config

--- configuration.nix
+++ configuration.nix
@@ -1,5 +1,5 @@
 { config, pkgs, ... }: {
-imports = [ ./hardware-configuration.nix ./conf/default.nix ];
+imports = [ ./hardware-configuration.nix ./hardware-overrides.nix ./conf/default.nix ];
 nixpkgs.config = import ./conf/nixpkgs.nix;
 _module.args.expr = import ./expr { inherit pkgs; };
 }
