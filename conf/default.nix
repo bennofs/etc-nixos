bd8879a930438acbb0612810183c64e8a2640d1c
Benno Fünfstück <benno.fuenfstueck@gmail.com>
2015-10-18 12:16:32 +0200

    allow normal users to mount devices

--- conf/default.nix
+++ conf/default.nix
@@ -109,6 +109,20 @@ services.journald.extraConfig = ''
   Storage=volatile
 '';
 
+# Allow normal users to mount devices
+security.polkit.enable = true;
+security.polkit.extraConfig = ''
+  polkit.addRule(function(action, subject) {
+    var YES = polkit.Result.YES;
+    var permission = {
+      "org.freedesktop.udisks2.filesystem-mount": YES,
+      "org.freedesktop.udisks2.filesystem-mount-system": YES,
+      "org.freedesktop.udisks2.eject-media": YES
+    };
+    return permission[action.id];
+  });
+'';
+
 nix = {
   useChroot = true;
   extraOptions = ''
