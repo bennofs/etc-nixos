69bda546bbf30c8a88f42e81d6feb59f5b1e9781
Benno Fünfstück <benno.fuenfstueck@gmail.com>
2016-01-09 12:55:21 +0100

    add laptop hardware overrides

---
+++ system/hardware-overrides-laptop.nix
@@ -0,0 +1,24 @@
+{ ... }: {
+
+hardware.cpu.intel.updateMicrocode = true;
+
+boot.loader.grub.device = "/dev/sdb";
+boot.loader.grub.extraEntries = ''
+  menuentry "Windows 10" {
+    chainloader (hd0,msdos1)+1;
+  }
+'';
+
+fileSystems."/data" = {
+  label = "data";
+  fsType = "ext4";
+};
+
+fileSystems."/data/code" = {
+  label = "code";
+  fsType = "ext4";
+};
+
+networking.wireless.interfaces = [ "wlp2s0" ];
+
+}
