df9ce2528edb4207e2c9734013a02936b7412a03
Benno Fünfstück <benno.fuenfstueck@gmail.com>
2016-06-15 23:23:46 +0200

    fix GTK3 theming

--- conf/desktop.nix
+++ conf/desktop.nix
@@ -19,11 +19,11 @@ let
   };
   themeEnv = ''
     # GTK2 theme
-    export GTK_PATH=$GTK_PATH:${pkgs.gtk-engine-murrine}/lib/gtk-2.0
-    export GTK2_RC_FILES=${pkgs.writeText "iconrc" ''gtk-icon-theme-name="breeze"''}:${pkgs.orion}/share/themes/orion/gtk-2.0/gtkrc:$GTK2_RC_FILES
+    export GTK2_RC_FILES=${pkgs.writeText "iconrc" ''gtk-icon-theme-name="breeze"''}:${pkgs.kde5.breeze}/share/themes/Breeze/gtk-2.0/gtkrc:$GTK2_RC_FILES
 
     # GTK3 theme
-    export GTK_THEME="orion"
+    export GTK_THEME="Breeze-gtk"
+    export XDG_DATA_DIRS="${pkgs.gnome-breeze}/share:$XDG_DATA_DIRS"
 
     # SVG loader for pixbuf (needed for svg icon themes)
     export GDK_PIXBUF_MODULE_FILE=$(echo ${pkgs.librsvg.out}/lib/gdk-pixbuf-2.0/*/loaders.cache)
