9e65cc0a38435d17d8dacc142fac43d8a2f8e1b7
Benno Fünfstück <benno.fuenfstueck@gmail.com>
2016-09-09 14:42:59 +0200

    fix armagetronad derivation build error

--- expr/armagetronad/default.nix
+++ expr/armagetronad/default.nix
@@ -1,4 +1,4 @@
-{stdenv, fetchbzr, boost, which, automake, autoconf, SDL, SDL_mixer, SDL_image, libxml2, protobuf, mesa, ftgl, glew, pkgconfig, libpng, m4, yacc, python}:
+{stdenv, fetchbzr, boost, which, automake, autoconf, SDL, SDL_mixer, SDL_image, libxml2, protobuf, mesa, ftgl, glew, pkgconfig, libpng, m4, yacc, python, freetype}:
 
 stdenv.mkDerivation rec {
   name = "armagetronad-0.4-bzr-r${rev}";
@@ -8,7 +8,7 @@ stdenv.mkDerivation rec {
     inherit rev;
     sha256 = "0hba1mg0rv9mnnahf6ks6d20v0801ass3r1vvijfmpf0sl0s8z7g";
   };
-  buildInputs = [stdenv boost which automake autoconf SDL SDL_mixer SDL_image libxml2 protobuf mesa ftgl glew pkgconfig libpng m4 yacc python];
+  buildInputs = [stdenv boost which automake autoconf SDL SDL_mixer SDL_image libxml2 protobuf mesa ftgl glew pkgconfig libpng m4 yacc python freetype];
   patchFlags = "-p0";
   configureFlags = "--disable-games --disable-etc";
   preConfigure = ''
