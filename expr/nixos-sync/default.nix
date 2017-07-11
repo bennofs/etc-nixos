70f95a7c54f95b03fe02074a98befc94ad47d108
Benno Fünfstück <benno.fuenfstueck@gmail.com>
2017-07-11 21:19:05 +0200

    fix nixos-sync for new wrapper path

--- expr/nixos-sync/default.nix
+++ expr/nixos-sync/default.nix
@@ -5,7 +5,7 @@ stdenv.mkDerivation {
   buildCommand = ''
     cat >$out <<EOF
     #!${stdenv.shell}
-    export PATH=/var/setuid-wrappers:${coreutils}/bin:${gnugrep}/bin:${systemd}/bin:${gnused}/bin:${less}/bin:${git}/bin:${nixos-rebuild}/bin
+    export PATH=/run/wrappers/bin:${coreutils}/bin:${gnugrep}/bin:${systemd}/bin:${gnused}/bin:${less}/bin:${git}/bin:${nixos-rebuild}/bin
     exec ${stdenv.shell} ${./nixos-sync.sh} "\$@"
     EOF
     chmod +x $out
