55693b8d9dfa671d908c8f75da9eb5dba29a7c4c
Benno Fünfstück <benno.fuenfstueck@gmail.com>
2017-07-15 23:21:37 +0200

    remove feh bg setting (use ~/.background-image)

--- conf/desktop.nix
+++ conf/desktop.nix
@@ -67,7 +67,6 @@ services.xserver = {
           ${expr.lock}/bin/lock
           ${expr.lock-suspend}/bin/lock-on-suspend &
 
-          ${pkgs.feh}/bin/feh --bg-fill "/data/pics/wallpapers/unsplash/autumn.jpg"
           ${pkgs.haskellPackages.xmobar}/bin/xmobar --dock --alpha 200 &
           ${pkgs.stalonetray}/bin/stalonetray --slot-size 22 --icon-size 20 --geometry 9x1-0 --icon-gravity NE --grow-gravity E -c /dev/null --kludges fix_window_pos,force_icons_size,use_icons_hints --transparent --tint-level 200 &> /dev/null &
           ${pkgs.xlibs.xrdb}/bin/xrdb -load ${./Xresources}
